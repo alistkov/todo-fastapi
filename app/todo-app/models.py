1c347efceb2e3b5957ef841cc53210a4581b07b0
alistkov <antonlistkov@gmail.com>
2025-08-11 22:39:17 +0200

    update todo model

--- app/todo-app/models.py
+++ app/todo-app/models.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Boolean, Column, Integer, String, TIMESTAMP, text
-from .database import Base
+from database import Base
 
 class Todo(Base):
   __tablename__ = 'todos'
 
   id = Column(Integer, primary_key=True, index=True)
   title = Column(String, index=True)
-  done = Column(Boolean, default=False)
+  description = Column(String)
+  proiority = Column(Integer)
+  complete = Column(Boolean, default=False)
   created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
   updated_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
