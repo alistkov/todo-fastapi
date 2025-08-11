0a5a1b7114e9167cf2bc1427ac632eb7ab643e8d
alistkov <antonlistkov@gmail.com>
2025-08-11 23:55:10 +0200

    add endpoint for get todo by id

--- app/todo_app/routers.py
+++ app/todo_app/routers.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Path
+from fastapi.exceptions import HTTPException
+from starlette import status
 
 from .models import Todo
 from .database import db_dependency
@@ -9,6 +10,13 @@ router = APIRouter(
     tags=["Todos"]
 )
 
-@router.get('/')
+@router.get('/', status_code=status.HTTP_200_OK)
 async def get_all(db: db_dependency):
     return db.query(Todo).all()
+
+@router.get('/{id}', status_code=status.HTTP_200_OK)
+async def get_todo_by_id(db: db_dependency, id: int = Path(gt=0)):
+    todo_model = db.query(Todo).filter(Todo.id == id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail=f'Todo with #{id} not found')
