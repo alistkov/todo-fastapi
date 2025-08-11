388eec60beddacba888644e58b1b0c133f110342
alistkov <antonlistkov@gmail.com>
2025-08-11 23:12:48 +0200

    fix typo

--- app/todo_app/models.py
+++ app/todo_app/models.py
@@ -7,7 +7,7 @@ class Todo(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String)
-    proiority = Column(Integer)
+    priority = Column(Integer)
     complete = Column(Boolean, default=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
     updated_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"))
